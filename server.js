3a6717bebc9000b000e861c7081dbb93f7c570f7
DuStein <steindu@gmail.com>
2022-12-09 22:33:52 -0300

    criei rota sucesso

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
   res.send('Servidor AWS Express OnLine!')
 })
 
+app.get('/sucesso', (req, res) => {
+  res.send('Acessando rota SUCESSO da app')
+})
+
 app.listen(3030, () => { console.log('Servidor na porta 3030')})
